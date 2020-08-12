08eb560c2dad6085455c8dcaa9717161101699cb
Erik Reyna <erik.reyna@washpost.com>
2020-08-12 17:27:49 -0600

    update title and text

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>Create Next App</title>
+        <title>Slacker starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="p-4 shadow rounded bg-white">
-        <h1 className="text-purple-500 leading-normal">Next.js</h1>
-        <p className="text-gray-500">with Tailwind CSS</p>
+        <h1 className="text-purple-500 leading-normal">Slacker starter</h1>
+        <p className="text-gray-500">Next.js + Tailwind CSS ❤️</p>
       </div>
     </Fragment>
   );
