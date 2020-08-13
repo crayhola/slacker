a305f46c02bb3b98e5ed01e78ccedbbbedbf3a3a
Erik Reyna <erikreyna@users.noreply.github.com>
2020-08-12 18:07:06 -0600

    feat: add vercel sponsorship requirements

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,26 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <Fragment>
+    <div className="flex flex-col min-h-screen py-1">
       <Head>
         <title>Slacker starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="p-4 shadow rounded bg-white">
+      <main className="flex-grow">
         <h1 className="text-purple-500 leading-normal">Slacker starter</h1>
         <p className="text-gray-500">Next.js + Tailwind CSS ❤️</p>
-      </div>
-    </Fragment>
+      </main>
+      <footer>
+        <div id="vercel-sponsorship">
+          Powered by{" "}
+          <img
+            className="object-contain h-4 inline-block"
+            src="/vercel.svg"
+            alt="Sponsored and powered by Vercel"
+          />
+        </div>
+      </footer>
+    </div>
   );
 }
