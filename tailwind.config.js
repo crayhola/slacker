65f415d28959b3f7318d28d51e5c3ad9c51b8def
lobenichou <lobenichou@protonmail.com>
2021-08-05 20:03:38 -0700

    fix build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
-      sans: ['Work Sans', 'helvetica', 'sans-serif'],
-    },
+      sans: ['Work Sans', 'helvetica', 'sans-serif']
+    }
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
