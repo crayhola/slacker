e52d504be9afd9ad1f5de69091d787d6b214a2a2
Aaron Williams <856628+aboutaaron@users.noreply.github.com>
2021-08-09 13:12:50 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
 
           <p>It’s been more than five years since we’ve started this community and it has ballooned to a size we never would have dreamed of. As demand for a community like ours expands, we feel the need to change how we administer invitations to the space. We’re revamping the invitation system as well as our website.</p>
 
-          <p><span className="underline">Here’s what this means:</span> we’re going to pause the application process while we're doing this work. But we’ll be back very soon! If you've applied before this hiatus, don't worry. You will hear from us very soon.</p>
+          <p><span className="underline">Here’s what this means:</span> we’re going to pause the signup process while we're doing this work. But we’ll be back very soon! If you've signed up before this hiatus, don't worry. You will hear from us very soon.</p>
 
           <p className="italic">-The JOC slack admins</p>
         </div>
